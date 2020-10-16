8c717a0e5fdc221b31fe2b3e7e522dc25aa77227
zhouchangjin <iamzhouchangjin@163.com>
2020-10-16 13:30:02 +0800

    add new classs

---
+++ include/C2DGameScene.h
@@ -0,0 +1,17 @@
+#ifndef C2DGAMESCENE_H
+#define C2DGAMESCENE_H
+
+#include <CGameScene.h>
+
+class C2DGameScene :public CGameScene
+{
+    public:
+        C2DGameScene();
+        virtual ~C2DGameScene();
+
+    protected:
+
+    private:
+};
+
+#endif // C2DGAMESCENE_H
---
+++ src/C2DGameScene.cpp
@@ -0,0 +1,11 @@
+#include "C2DGameScene.h"
+
+C2DGameScene::C2DGameScene()
+{
+    //ctor
+}
+
+C2DGameScene::~C2DGameScene()
+{
+    //dtor
+}
